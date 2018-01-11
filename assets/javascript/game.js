9e590b0ad42aea2541245966933ad7eb767ffd37
rmarro <rmarro47@gmail.com>
2018-01-10 23:07:15 -0600

    updated game pseudocode, started initial bootstrap layout

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,5 @@
-// how can you only allow a player to interact with their buttons???
+// how can you only allow a player to see and interact with their buttons, and
+    // only on their turn???
 // how can you only have changes displayed (rps selection before reveal) for
     //the person who chose it? 
         // save selection to firebase but firebase doesn't do anything else to
@@ -9,25 +10,18 @@
     // how restrict online presense to 2?
     // assigned p1 or p2
     // update firebase with player name, number
-    // update html with name, waiting for other player if not 2
-    // once 2, "let's play!" and buttons show
+    // update html with name
+    // once 2, update turn and button options show
 
 // 2. Gameplay
-    // Record players' choices
-        // where? under their player node? in a new round 1 node?
+    // Turn node to show if it's p1 or p2 turn 
+        // Record players' choices
     // Logic to determine win
-        // how?? javascript function triggered by addChild/value
+        // how?? javascript function triggered by addChild/value?
     // Display choices and winner
-    // Increase score
-        // saved in player node? set() for score key?
-    // Increase round
+    // Increase wins and losses accordingly
+        // saved in player node-- set() for wins/losses keys?
     // Reset buttons and play the next round
-
-// 3. Game over
-    // When 3 rounds are done, look at scores in firebase
-    // whoever's is higher wins
-    // display message
-    // allow play again if both players select
 
 // 4. Chat 
     // Save usernames
