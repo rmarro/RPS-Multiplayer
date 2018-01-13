1726a04a86d1edd71fc8f372bf7ce4037149c12b
rmarro <rmarro47@gmail.com>
2018-01-12 22:43:43 -0600

    added panels; logic for adding player 1 and 2 to firebase; names into dom

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,7 +7,6 @@
 
 // 1. Get 2 players online and saved as nodes in firebase
 // Player is asked for their name, or told game is full if already 2
-    // how restrict online presense to 2?
     // assigned p1 or p2
     // update firebase with player name, number
     // update html with name
@@ -27,3 +26,72 @@
     // Save usernames
     // Push messages to firebase
         // Look at Saturday class and online resources to start this
+
+//=======================================================
+
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAegjt31NE8XeeONMMmt7akANPi26aTpaM",
+    authDomain: "rps-game-41b05.firebaseapp.com",
+    databaseURL: "https://rps-game-41b05.firebaseio.com",
+    projectId: "rps-game-41b05",
+    storageBucket: "",
+    messagingSenderId: "1042427264201"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+var playersRef = database.ref("/players");
+
+// var connectionsRef = database.ref("/connections");
+// var connectedRef = database.ref(".info/connected");
+// connectedRef.on("value", function(snap) {
+//     if (snap.val()) {
+//         var con = connectionsRef.push(true);
+//         con.onDisconnect().remove();
+//     }
+// });
+
+
+// Add player on submit button
+$("#add-name-button").on("click", function(){
+    event.preventDefault();
+    var name = $("#name-input").val();
+    var player = {
+        name: name,
+        losses: 0,
+        wins: 0,
+    };
+    // When a player is added, see if they're 1st or 2nd
+    playersRef.once("value").then(function(snapshot){
+        var playerNumber = snapshot.numChildren();
+        if (playerNumber == 0) {
+            playersRef.child("1").set(player);
+        } 
+        else if (playerNumber == 1) {
+            playersRef.child("2").set(player);
+        } 
+        else {
+            console.log("game full")
+        }
+    });
+});
+
+// When a player is added, put their name in panel headings
+playersRef.on("child_added", function(snapshot, prevChildKey){
+    playerObject = snapshot.val();
+    console.log(prevChildKey);
+    if (prevChildKey === "1") {
+        $("#p2-heading").text("Player 2: " + playerObject.name);
+    } else {
+        $("#p1-heading").text("Player 1: " + playerObject.name);
+    }
+});
+
+
+
+
+
+
